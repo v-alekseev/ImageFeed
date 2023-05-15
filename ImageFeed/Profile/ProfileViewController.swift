ab0dc4cffbaacdd16f71d688c5012cbfdcf73333
Vitaly Alekseev <valekseev@yandex.ru>
2023-05-15 22:59:28 +0300

    add ProfileViewController and Profile screen

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Vitaly Alekseev on 15.05.2023.
+//
+
+import Foundation
+import UIKit
+
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
